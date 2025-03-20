d2aced6aebdafb113c65afa809158b619c67d612
typhoon <jungwooshin.97@gmail.com>
2025-03-20 19:02:56 +0900

    init

---
+++ src/test/java/com/zipline/ZiplineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiplineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
