852900371a29451e078a4dfad642326c94212c47
Dsys1129 <dsys1129@gmail.com>
2025-03-25 12:16:32 +0900

    feat: 공통 Success 응답 포맷 구현

---
+++ src/main/java/com/zipline/global/common/response/ApiResponse.java
@@ -0,0 +1,47 @@
+package com.zipline.global.common.response;
+
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Getter;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+	private boolean success;
+	private int code;
+	private String message;
+	private T data;
+
+	private ApiResponse(boolean success, int code, String message, T data) {
+		this.success = success;
+		this.code = code;
+		this.message = message;
+		this.data = data;
+	}
+
+	public static <T> ApiResponse<T> success(HttpStatus status, String message, T data) {
+		return new ApiResponse<>(true, status.value(), message, data);
+	}
+
+	public static <T> ApiResponse<T> success(HttpStatus status, String message) {
+		return new ApiResponse<>(true, status.value(), message, null);
+	}
+
+	public static <T> ApiResponse<T> ok(String message, T data) {
+		return new ApiResponse<>(true, 200, message, data);
+	}
+
+	public static <T> ApiResponse<T> ok(String message) {
+		return new ApiResponse<>(true, 200, message, null);
+	}
+
+	public static <T> ApiResponse<T> create(String message, T data) {
+		return new ApiResponse<>(true, 201, message, data);
+	}
+
+	public static <T> ApiResponse<T> create(String message) {
+		return new ApiResponse<>(true, 201, message, null);
+	}
+}
