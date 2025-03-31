3c24a0ed05e5cfe8a67421de823b13efb459b68d
suyeon <whtndus0809@naver.com>
2025-03-31 16:49:36 +0900

    fix: @Param 추가

--- src/main/java/com/zipline/repository/UserRepository.java
+++ src/main/java/com/zipline/repository/UserRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.zipline.entity.User;
 
+import io.lettuce.core.dynamic.annotation.Param;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 	@Query("SELECT user FROM User user WHERE user.id = :id")
-	Optional<User> findByLoginId(String id);
+	Optional<User> findByLoginId(@Param("id") String id);
 
 	boolean existsById(String id);
 }
