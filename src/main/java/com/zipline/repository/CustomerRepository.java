956a6b6cb4954698c083f7e008453a8292452ba3
Dsys1129 <dsys1129@gmail.com>
2025-04-01 14:20:35 +0900

    feat: CustomerRepository - findByUidAndIsDeletedFalse Query 추가 삭제되지 않은 고객을 UID로 검색

--- src/main/java/com/zipline/repository/CustomerRepository.java
+++ src/main/java/com/zipline/repository/CustomerRepository.java
@@ -1,5 +1,7 @@
 package com.zipline.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.zipline.entity.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+	Optional<Customer> findByUidAndIsDeletedFalse(Long customerUid);
 }
