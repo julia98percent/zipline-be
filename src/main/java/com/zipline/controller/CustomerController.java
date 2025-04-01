7357ec4b9d3608ec51d1baca7a00b793974cffe8
Dsys1129 <dsys1129@gmail.com>
2025-04-01 14:19:44 +0900

    feat: Customer 수정 Controller EndPoint 생성

--- src/main/java/com/zipline/controller/CustomerController.java
+++ src/main/java/com/zipline/controller/CustomerController.java
@@ -4,11 +4,15 @@ import java.security.Principal;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.zipline.dto.CustomerModifyRequestDTO;
+import com.zipline.dto.CustomerModifyResponseDTO;
 import com.zipline.dto.CustomerRegisterRequestDTO;
 import com.zipline.global.common.response.ApiResponse;
 import com.zipline.service.CustomerService;
@@ -27,10 +31,19 @@ public class CustomerController {
 
 	@PostMapping("/customers")
 	public ResponseEntity<ApiResponse<Void>> registerCustomer(
-		@Valid @RequestBody CustomerRegisterRequestDTO customerRegisterRequestDTO,
-		Principal principal) {
+		@Valid @RequestBody CustomerRegisterRequestDTO customerRegisterRequestDTO, Principal principal) {
+
 		ApiResponse<Void> response = customerService.registerCustomer(customerRegisterRequestDTO,
 			Long.parseLong(principal.getName()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+
+	@PutMapping("/customers/{customerUid}")
+	public ResponseEntity<ApiResponse<CustomerModifyResponseDTO>> modifyCustomer(@PathVariable Long customerUid,
+		@Valid @RequestBody CustomerModifyRequestDTO customerModifyRequestDTO, Principal principal) {
+
+		ApiResponse<CustomerModifyResponseDTO> response = customerService.modifyCustomer(
+			customerUid, customerModifyRequestDTO, Long.parseLong(principal.getName()));
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 }
