11795d0340bc7f61220e8f4c35acfd590514a95c
typhoon <jungwooshin.97@gmail.com>
2025-04-01 16:57:11 +0900

    (refactor)고정값 상수화

--- src/main/java/com/zipline/util/CoordinateUtil.java
+++ src/main/java/com/zipline/util/CoordinateUtil.java
@@ -3,6 +3,10 @@ package com.zipline.util;
 public class CoordinateUtil {
     private static final double EARTH_RADIUS = 6371.0; // 지구 반경 (km)
     
+    // 줌 레벨 12,14의 위도/경도 변화량
+    private static final double BASE_LAT_DELTA = 0.13173545;
+    private static final double BASE_LON_DELTA = 0.16033175;
+    
     /**
      * 주어진 위도, 경도, 줌 레벨에 대한 지리적 범위를 계산합니다.
      * 
@@ -12,24 +16,19 @@ public class CoordinateUtil {
      * @return [top, right, bottom, left] 순서의 좌표 배열
      */
     public static double[] calculateBounds(double lat, double lon, int zoom) {
-        // 줌 레벨에 따른 위도/경도 변화량 계산
-        // 줌 레벨 12,14 기준으로 계산
-        double baseLatDelta = 0.13173545; // 줌 레벨 12,14의 위도 변화량
-        double baseLonDelta = 0.16033175; // 줌 레벨 12,14의 경도 변화량
-        
         // 줌 레벨에 따른 스케일 조정
         double scale = Math.pow(2, 12 - zoom);
-        double latDelta = baseLatDelta * scale;
+        double latDelta = BASE_LAT_DELTA * scale;
         
         // 경도 변화량 계산 (위도에 따른 보정)
         double latFactor = Math.cos(Math.toRadians(lat));
-        double lonDelta = baseLonDelta * scale * latFactor;
+        double lonDelta = BASE_LON_DELTA * scale * latFactor;
         
         // 중심점 기준으로 상하좌우 범위 계산
         double top = lat + latDelta;
+        double right = lon + lonDelta;
         double bottom = lat - latDelta;
         double left = lon - lonDelta;
-        double right = lon + lonDelta;
         
         return new double[] { top, right, bottom, left };
     }
