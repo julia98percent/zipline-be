2e0a95f4f82cdc1dd4430b04f783894defcf8d19
Dsys1129 <dsys1129@gmail.com>
2025-04-01 14:23:38 +0900

    feat: 고객 수정 Request 및 ResponseDTO 생성

---
+++ src/main/java/com/zipline/dto/CustomerModifyResponseDTO.java
@@ -0,0 +1,48 @@
+package com.zipline.dto;
+
+import java.math.BigInteger;
+
+import com.zipline.entity.Customer;
+
+import lombok.Getter;
+
+@Getter
+public class CustomerModifyResponseDTO {
+	private Long uid;
+	private String name;
+	private String phoneNo;
+	private String address;
+	private String telProvider;
+	private String region;
+	private BigInteger minRent;
+	private BigInteger maxRent;
+	private String trafficSource;
+	private boolean isLandlord;
+	private boolean isTenant;
+	private boolean isBuyer;
+	private boolean isSeller;
+	private BigInteger maxPrice;
+	private BigInteger minPrice;
+	private BigInteger minDeposit;
+	private BigInteger maxDeposit;
+
+	public CustomerModifyResponseDTO(Customer customer) {
+		this.uid = customer.getUid();
+		this.name = customer.getName();
+		this.phoneNo = customer.getPhoneNo();
+		this.address = customer.getAddress();
+		this.telProvider = customer.getTelProvider();
+		this.region = customer.getRegion();
+		this.minRent = customer.getMinRent();
+		this.maxRent = customer.getMaxRent();
+		this.trafficSource = customer.getTrafficSource();
+		this.isLandlord = customer.isLandlord();
+		this.isTenant = customer.isTenant();
+		this.isBuyer = customer.isBuyer();
+		this.isSeller = customer.isSeller();
+		this.maxPrice = customer.getMaxPrice();
+		this.minPrice = customer.getMinPrice();
+		this.minDeposit = customer.getMinDeposit();
+		this.maxDeposit = customer.getMaxDeposit();
+	}
+}
