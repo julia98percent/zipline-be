72bfe53c31cb9eb071b4c03fc108a35d22fb63b5
suyeon <whtndus0809@naver.com>
2025-04-06 19:25:10 +0900

    fix: 응답 메시지 수정

--- src/main/java/com/zipline/controller/PropertyController.java
+++ src/main/java/com/zipline/controller/PropertyController.java
@@ -6,12 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.zipline.dto.AgentPropertyRequestDTO;
 import com.zipline.dto.AgentPropertyResponseDTO;
 import com.zipline.global.common.response.ApiResponse;
 import com.zipline.service.AgentPropertyService;
@@ -30,15 +27,7 @@ public class PropertyController {
 		Principal principal) {
 		AgentPropertyResponseDTO propertyResponseDTO = agentPropertyService.getProperty(propertyUid,
 			Long.parseLong(principal.getName()));
-		ApiResponse<AgentPropertyResponseDTO> response = ApiResponse.ok("매물 조회 성공", propertyResponseDTO);
+		ApiResponse<AgentPropertyResponseDTO> response = ApiResponse.ok("매물 상세 조회 성공", propertyResponseDTO);
 		return ResponseEntity.status(HttpStatus.OK).body(response);
-	}
-
-	@PostMapping("")
-	public ResponseEntity<ApiResponse<Void>> registerProperty(
-		@RequestBody AgentPropertyRequestDTO agentPropertyRequestDTO, Principal principal) {
-		agentPropertyService.registerProperty(agentPropertyRequestDTO, Long.parseLong(principal.getName()));
-		ApiResponse<Void> response = ApiResponse.create("매물 등록 성공");
-		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 }
