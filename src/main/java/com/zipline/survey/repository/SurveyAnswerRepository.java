cfad512356df15108c8ae89992123d2bb5258250
Dsys1129 <dsys1129@gmail.com>
2025-04-08 16:06:57 +0900

    feat: 제출된 설문 조회 로직 구현 - SurveyResponse, SurveyAnswer 조회 시 Fetch Join 적용하여 N+1 문제 방지

--- src/main/java/com/zipline/survey/repository/SurveyAnswerRepository.java
+++ src/main/java/com/zipline/survey/repository/SurveyAnswerRepository.java
@@ -1,8 +1,14 @@
 package com.zipline.survey.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.zipline.survey.entity.SurveyAnswer;
 
 public interface SurveyAnswerRepository extends JpaRepository<SurveyAnswer, Long> {
+
+	@Query("SELECT sa FROM SurveyAnswer sa JOIN FETCH sa.question q LEFT JOIN FETCH q.choices WHERE sa.surveyResponse.uid = :surveyResponseUid")
+	List<SurveyAnswer> findByWithQuestionsAndChoicesSurveyResponseUid(Long surveyResponseUid);
 }
