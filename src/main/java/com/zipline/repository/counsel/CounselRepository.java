caa5b86ad915c0e9cab519d85475e0a2d7a7bf68
Dsys1129 <dsys1129@gmail.com>
2025-04-10 16:55:05 +0900

    feat: 고객 상세 조회 - 상담 내역 조회 로직 구현

--- src/main/java/com/zipline/repository/counsel/CounselRepository.java
+++ src/main/java/com/zipline/repository/counsel/CounselRepository.java
@@ -1,5 +1,6 @@
 package com.zipline.repository.counsel;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,6 @@ import com.zipline.entity.counsel.Counsel;
 
 public interface CounselRepository extends JpaRepository<Counsel, Long> {
 	Optional<Counsel> findByUidAndDeletedAtIsNull(Long uid);
+
+	List<Counsel> findByCustomerUidAndUserUidAndDeletedAtIsNullOrderByCreatedAtDesc(Long customerUid, Long userUid);
 }
