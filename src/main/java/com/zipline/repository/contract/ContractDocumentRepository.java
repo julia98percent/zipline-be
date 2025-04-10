388bde4a3a915be8977256e682014ed9904f16d9
suyeon <whtndus0809@naver.com>
2025-04-10 22:58:13 +0900

    fix: 계약 repository @Repository 어노테이션 추가

--- src/main/java/com/zipline/repository/contract/ContractDocumentRepository.java
+++ src/main/java/com/zipline/repository/contract/ContractDocumentRepository.java
@@ -3,10 +3,12 @@ package com.zipline.repository.contract;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.zipline.entity.contract.Contract;
 import com.zipline.entity.contract.ContractDocument;
 
+@Repository
 public interface ContractDocumentRepository extends JpaRepository<ContractDocument, Long> {
 	List<ContractDocument> findAllByContract(Contract contract);
 }
--- src/main/java/com/zipline/repository/contract/CustomerContractRepository.java
+++ src/main/java/com/zipline/repository/contract/CustomerContractRepository.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.zipline.entity.contract.Contract;
 import com.zipline.entity.contract.CustomerContract;
 
+@Repository
 public interface CustomerContractRepository extends JpaRepository<CustomerContract, Long> {
 	@Query("SELECT cc FROM CustomerContract cc Where cc.contract.uid IN :contractIds ORDER BY cc.contract.uid DESC")
 	List<CustomerContract> findInContractUids(List<Long> contractIds);
