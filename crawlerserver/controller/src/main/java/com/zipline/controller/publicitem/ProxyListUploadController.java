ab6f550bf4b5c8c5c48d0861dbec3753473024e4
suyeon <whtndus0809@naver.com>
2025-04-21 19:33:05 +0900

    refactor: Service 레이어 인터페이스화
    crawlerserver 모듈 내 service 레이어 인터페이스화

--- crawlerserver/controller/src/main/java/com/zipline/controller/publicitem/ProxyListUploadController.java
+++ crawlerserver/controller/src/main/java/com/zipline/controller/publicitem/ProxyListUploadController.java
@@ -1,6 +1,5 @@
 package com.zipline.controller.publicitem;
 
-import com.zipline.global.response.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,23 +7,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-
-import com.zipline.service.publicItem.ProxyListUploadService;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.zipline.global.response.ApiResponse;
+import com.zipline.service.publicItem.ProxyListUploadService;
 
 @RestController
 @RequestMapping("/api/admin/upload-proxy-list")
 public class ProxyListUploadController {
 
-    @Autowired
-    private ProxyListUploadService proxyListUploadService;
+	@Autowired
+	private ProxyListUploadService proxyListUploadService;
 
-    @PostMapping
-    public ResponseEntity<ApiResponse<String>> uploadProxyList(@RequestParam("file") MultipartFile file) {
-            String result = proxyListUploadService.saveFile(file);
-            ApiResponse<String> response = ApiResponse.ok("파일 업로드 성공", result);
-            return ResponseEntity.status(HttpStatus.OK).body(response); 
-    }
+	@PostMapping
+	public ResponseEntity<ApiResponse<String>> uploadProxyList(@RequestParam("file") MultipartFile file) {
+		String result = proxyListUploadService.saveFile(file);
+		ApiResponse<String> response = ApiResponse.ok("파일 업로드 성공", result);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
 }
 
