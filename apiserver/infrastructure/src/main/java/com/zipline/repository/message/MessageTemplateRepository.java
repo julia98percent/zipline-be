f667b5c4502fcdbf42f6d22aad0a6fbf4c88efd3
채지원 <dev.julia.chae@gmail.com>
2025-04-23 11:54:38 +0900

    feat: 문자 템플릿 repository 추가

--- apiserver/infrastructure/src/main/java/com/zipline/repository/message/MessageTemplateRepository.java
+++ apiserver/infrastructure/src/main/java/com/zipline/repository/message/MessageTemplateRepository.java
@@ -1,7 +1,10 @@
 package com.zipline.repository.message;
 
+import com.zipline.entity.enums.MessageTemplateCategory;
 import com.zipline.entity.message.MessageTemplate;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageTemplateRepository  extends JpaRepository<MessageTemplate, Long> {
+  Optional<MessageTemplate> findByCategoryAndUserUidAndDeletedAtIsNull(MessageTemplateCategory category, Long userUid);
 }
