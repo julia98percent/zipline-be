fcd4124db5cae372af8251a9e60076cfcdd2daed
suyeon <whtndus0809@naver.com>
2025-04-24 15:58:26 +0900

    fix: 에러메시지 수정

--- apiserver/service/src/main/java/com/zipline/service/label/LabelServiceImpl.java
+++ apiserver/service/src/main/java/com/zipline/service/label/LabelServiceImpl.java
@@ -24,7 +24,7 @@ public class LabelServiceImpl implements LabelService {
 	@Transactional
 	public void createLabel(Long userUid, LabelRequestDTO labelRequestDTO) {
 		User user = userRepository.findById(userUid)
-			.orElseThrow(() -> new UserNotFoundException("사용자 없음", HttpStatus.BAD_REQUEST));
+			.orElseThrow(() -> new UserNotFoundException("유저를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST));
 		boolean exists = labelRepository.existsByUserUidAndName(userUid, labelRequestDTO.getName());
 		if (exists) {
 			throw new LabelNameDuplicatedException("이미 존재하는 라벨 이름입니다.", HttpStatus.BAD_REQUEST);
