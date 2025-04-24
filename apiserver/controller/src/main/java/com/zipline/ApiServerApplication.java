6fb6bb28c5db981d20dce62d165d2b1a5c099b48
Dsys1129 <dsys1129@gmail.com>
2025-04-24 12:30:01 +0900

    feat: ApiServerApplication - EnableJpaAuditing Annotation 추가

--- apiserver/controller/src/main/java/com/zipline/ApiServerApplication.java
+++ apiserver/controller/src/main/java/com/zipline/ApiServerApplication.java
@@ -2,11 +2,13 @@ package com.zipline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ApiServerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ApiServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServerApplication.class, args);
+	}
 }
 
