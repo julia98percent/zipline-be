30236fe48374ffb0a6f5424deb02aeda67f219d4
suyeon <whtndus0809@naver.com>
2025-04-24 18:49:20 +0900

    fix: 입력값 조건 및 메시지 수정

--- apiserver/service/src/main/java/com/zipline/service/label/dto/request/LabelRequestDTO.java
+++ apiserver/service/src/main/java/com/zipline/service/label/dto/request/LabelRequestDTO.java
@@ -16,7 +16,7 @@ public class LabelRequestDTO {
 
 	@Schema(description = "라벨 이름", example = "중요")
 	@NotBlank(message = "라벨명은 필수 입력값입니다.")
-	@Size(min = 1, max = 10, message = "라벨명은 1자 이상 10자 이하로 입력해주세요.")
+	@Size(max = 10, message = "라벨명은 10자 이하로 입력해주세요.")
 	@Pattern(regexp = "^[a-zA-Z가-힣0-9\\s]+$", message = "라벨명에는 특수문자를 포함할 수 없습니다.")
 	private String name;
 
