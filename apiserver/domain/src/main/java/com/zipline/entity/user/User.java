33ee0095f29b630734f358b1df21c22d05814093
suyeon <whtndus0809@naver.com>
2025-04-24 12:06:47 +0900

    fix: 어노테이션 정리

--- apiserver/domain/src/main/java/com/zipline/entity/user/User.java
+++ apiserver/domain/src/main/java/com/zipline/entity/user/User.java
@@ -10,14 +10,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Getter
 @Entity
 @Table(name = "users")
