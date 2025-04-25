d0824d705e9886862f5cbda38e2475966d567481
suyeon <whtndus0809@naver.com>
2025-04-25 12:13:50 +0900

    feat: 라벨 수정 로직 구현

--- apiserver/controller/src/main/java/com/zipline/controller/label/LabelController.java
+++ apiserver/controller/src/main/java/com/zipline/controller/label/LabelController.java
@@ -3,6 +3,8 @@ package com.zipline.controller.label;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.zipline.global.response.ApiResponse;
 import com.zipline.service.label.LabelService;
 import com.zipline.service.label.dto.request.LabelRequestDTO;
+import com.zipline.service.label.dto.response.LabelResponseDTO;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -32,4 +35,14 @@ public class LabelController {
 		return ResponseEntity.ok(ApiResponse.create("라벨 생성 완료"));
 	}
 
+	@PatchMapping("/{labelUid}")
+	public ResponseEntity<ApiResponse<LabelResponseDTO>> modifyLabel(
+		@PathVariable Long labelUid,
+		@Valid @RequestBody LabelRequestDTO labelRequestDTO,
+		@AuthenticationPrincipal UserDetails userDetails
+	) {
+		Long userUid = Long.parseLong(userDetails.getUsername());
+		LabelResponseDTO responseDTO = labelService.modifyLabel(userUid, labelUid, labelRequestDTO);
+		return ResponseEntity.ok(ApiResponse.ok("라벨 수정 완료", responseDTO));
+	}
 }
--- apiserver/service/src/main/java/com/zipline/service/label/LabelService.java
+++ apiserver/service/src/main/java/com/zipline/service/label/LabelService.java
@@ -1,7 +1,10 @@
 package com.zipline.service.label;
 
 import com.zipline.service.label.dto.request.LabelRequestDTO;
+import com.zipline.service.label.dto.response.LabelResponseDTO;
 
 public interface LabelService {
 	void createLabel(Long userUid, LabelRequestDTO dto);
+
+	LabelResponseDTO modifyLabel(Long userUid, Long labelUid, LabelRequestDTO dto);
 }
