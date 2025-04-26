969ff0c4f0b8b327d5d55cfc5ea061ab2b5e4c7e
typhoon <jungwooshin.97@gmail.com>
2025-04-26 14:35:43 +0900

    (feat)readonly추가

--- apiserver/infrastructure/src/main/java/com/zipline/repository/region/RegionRepository.java
+++ apiserver/infrastructure/src/main/java/com/zipline/repository/region/RegionRepository.java
@@ -3,10 +3,12 @@ package com.zipline.repository.region;
 import com.zipline.entity.publicitem.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Long> {
+    @Transactional(readOnly = true)
     List<Region> findByParentCortarNo(Long parentCortarNo);
 }
