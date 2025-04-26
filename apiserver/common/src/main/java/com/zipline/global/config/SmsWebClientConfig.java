15501fb159018c65d9293626eb8fc2c84018985f
채지원 <dev.julia.chae@gmail.com>
2025-04-26 16:39:40 +0900

    fix: 문자 signature를 api 요청시마다 만들도록 수정

--- apiserver/common/src/main/java/com/zipline/global/config/SmsWebClientConfig.java
+++ apiserver/common/src/main/java/com/zipline/global/config/SmsWebClientConfig.java
@@ -1,39 +1,48 @@
 package com.zipline.global.config;
 
 import com.zipline.global.util.SmsSignatureGenerator;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 
 @Configuration
 public class SmsWebClientConfig {
 
+  private final SmsSignatureGenerator smsSignatureGenerator;
   @Value("${sms.api-key}")
   String apiKey;
 
+  public SmsWebClientConfig(SmsSignatureGenerator smsSignatureGenerator) {
+    this.smsSignatureGenerator = smsSignatureGenerator;
+  }
+
   @Bean
-  public WebClient webClient(SmsSignatureGenerator smsSignatureGenerator) {
-    Map<String, String> signatureResult = smsSignatureGenerator.generateSignature().block();
-
-    if (signatureResult == null || signatureResult.isEmpty()) {
-      throw new IllegalArgumentException("유효하지 않은 signature 값 입니다.");
-    }
-
-    String authorizationHeader = String.format(
-        "HMAC-SHA256 apiKey=%s, date=%s, salt=%s, signature=%s",
-        apiKey, signatureResult.get("time"), signatureResult.get("salt"),
-        signatureResult.get("hash")
-    );
-
+  public WebClient webClient() {
     return WebClient.builder()
         .baseUrl("https://api.solapi.com/messages/v4")
         .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-        .defaultHeader(HttpHeaders.AUTHORIZATION, authorizationHeader)
+        .filter(ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+          return smsSignatureGenerator.generateSignature()
+              .map(signatureResult -> {
+                String authorizationHeader = String.format(
+                    "HMAC-SHA256 apiKey=%s, date=%s, salt=%s, signature=%s",
+                    apiKey,
+                    signatureResult.get("time"),
+                    signatureResult.get("salt"),
+                    signatureResult.get("hash")
+                );
+
+                return ClientRequest.from(clientRequest)
+                    .header(HttpHeaders.AUTHORIZATION, authorizationHeader)
+                    .build();
+              });
+        }))
         .build();
   }
 }
