fba2c77c44f004d8b594c14f6bbb2a0ea12d09a5
Dsys1129 <dsys1129@gmail.com>
2025-04-26 19:07:17 +0900

    refactor: Survey Service - Data 관련 DTO만 반환하도록 변경

--- apiserver/service/src/main/java/com/zipline/service/survey/SurveyService.java
+++ apiserver/service/src/main/java/com/zipline/service/survey/SurveyService.java
@@ -7,7 +7,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.zipline.entity.user.User;
 import com.zipline.global.request.PageRequestDTO;
-import com.zipline.global.response.ApiResponse;
 import com.zipline.service.survey.dto.request.SurveyCreateRequestDTO;
 import com.zipline.service.survey.dto.request.SurveySubmitRequestDTO;
 import com.zipline.service.survey.dto.response.SurveyResponseDTO;
@@ -16,16 +15,15 @@ import com.zipline.service.survey.dto.response.SurveyResponseListDTO;
 
 public interface SurveyService {
 
-	Map<String, String> createSurvey(SurveyCreateRequestDTO requestDTO, Long agentUID);
+	Map<String, String> createSurvey(SurveyCreateRequestDTO requestDTO, Long userUid);
 
 	void createDefaultSurveyForUser(User user);
 
-	ApiResponse<SurveyResponseDTO> getSurvey(Long surveyUID);
+	SurveyResponseDTO getSurvey(Long surveyUid);
 
-	ApiResponse<Void> submitSurvey(Long surveyUid, List<SurveySubmitRequestDTO> requestDTOList,
-		List<MultipartFile> files);
+	void submitSurvey(Long surveyUid, List<SurveySubmitRequestDTO> requestDTOList, List<MultipartFile> files);
 
 	SurveyResponseListDTO getSurveyResponses(PageRequestDTO pageRequestDTO, Long userUid);
 
-	ApiResponse<SurveyResponseDetailDTO> getSubmittedSurvey(Long surveyResponseUid, Long userUid);
+	SurveyResponseDetailDTO getSubmittedSurvey(Long surveyResponseUid, Long userUid);
 }
