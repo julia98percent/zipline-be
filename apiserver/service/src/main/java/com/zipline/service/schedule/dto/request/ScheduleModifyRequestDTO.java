c6ba2f1fc01a7aa74b3657f7e44d27cbdd67015e
채지원 <dev.julia.chae@gmail.com>
2025-04-27 22:02:14 +0900

    feat: 일정 조회 요청시 필수값 validation 추가

--- apiserver/service/src/main/java/com/zipline/service/schedule/dto/request/ScheduleModifyRequestDTO.java
+++ apiserver/service/src/main/java/com/zipline/service/schedule/dto/request/ScheduleModifyRequestDTO.java
@@ -11,9 +11,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ScheduleModifyRequestDTO {
 
+  @NotNull(message = "시작 날짜는 필수 입력값입니다.")
   private LocalDateTime startDate;
+
+  @NotNull(message = "종료 날짜는 필수 입력값입니다.")
   private LocalDateTime endDate;
+
+  @NotBlank(message = "제목은 필수 입력값입니다.")
   private String title;
+
   private String description;
   private Long customerUid;
 }
