696d68cc764b33b66bdc9647d129f285d2021d1a
채지원 <dev.julia.chae@gmail.com>
2025-04-27 14:25:49 +0900

    chore: LocalDateTime 관련 중복 어노테이션 제거를 위한 JacksonConfig 수정

--- apiserver/service/src/main/java/com/zipline/service/schedule/dto/request/DateRangeRequest.java
+++ apiserver/service/src/main/java/com/zipline/service/schedule/dto/request/DateRangeRequest.java
@@ -4,16 +4,13 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @AllArgsConstructor
 public class DateRangeRequest {
-  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
   @NotNull(message = "시작 날짜는 필수입니다.")
   private LocalDateTime startDate;
 
-  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
   @NotNull(message = "종료 날짜는 필수입니다.")
   private LocalDateTime endDate;
 }
--- apiserver/service/src/main/java/com/zipline/service/schedule/dto/response/ScheduleResponseDTO.java
+++ apiserver/service/src/main/java/com/zipline/service/schedule/dto/response/ScheduleResponseDTO.java
@@ -1,31 +1,18 @@
 package com.zipline.service.schedule.dto.response;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.zipline.entity.customer.Customer;
 import com.zipline.entity.schedule.Schedule;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 public class ScheduleResponseDTO {
   private Long uid;
   private String title;
   private String description;
-
-  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-  @JsonSerialize(using = LocalDateTimeSerializer.class)
   private LocalDateTime startDate;
-
-  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-  @JsonSerialize(using = LocalDateTimeSerializer.class)
   private LocalDateTime endDate;
 
   private Long customerUid;
