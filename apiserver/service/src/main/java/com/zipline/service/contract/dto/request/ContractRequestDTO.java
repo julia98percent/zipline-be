bd4b0f955c97e82013c78c9bd46edb424c1003c7
suyeon <whtndus0809@naver.com>
2025-04-28 13:12:33 +0900

    fix: Contract 리펙토링

--- apiserver/service/src/main/java/com/zipline/service/contract/dto/request/ContractRequestDTO.java
+++ apiserver/service/src/main/java/com/zipline/service/contract/dto/request/ContractRequestDTO.java
@@ -2,49 +2,45 @@ package com.zipline.service.contract.dto.request;
 
 import java.time.LocalDate;
 
-import com.zipline.entity.contract.Contract;
-import com.zipline.entity.enums.ContractStatus;
-import com.zipline.entity.user.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Schema(description = "계약 생성 요청 DTO")
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class ContractRequestDTO {
 
 	@Schema(description = "계약 카테고리", example = "월세")
 	private String category;
 
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	@Schema(description = "계약일", example = "2025-03-10")
 	private LocalDate contractDate;
 
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	@Schema(description = "계약 시작일", example = "2025-04-01")
 	private LocalDate contractStartDate;
 
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	@Schema(description = "계약 종료일", example = "2026-04-01")
 	private LocalDate contractEndDate;
 
 	@Schema(description = "계약 상태", example = "PENDING")
-	private ContractStatus status;
+	private String status;
 
-	@Schema(description = "고객 UID", example = "1")
-	private Long customerUid;
+	@Schema(description = "임대/매도자 고객 UID", example = "1")
+	private Long lessorOrSellerUid;
 
+	@Schema(description = "임차/매수자 고객 UID", example = "2")
+	private Long lesseeOrBuyerUid;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	@Schema(description = "계약 종료 예상일", example = "2026-05-01")
 	private LocalDate expectedContractEndDate;
 
-	public Contract toEntity(User user) {
-		return Contract.builder()
-			.category(this.category)
-			.contractStartDate(this.contractStartDate)
-			.contractEndDate(this.contractEndDate)
-			.contractDate(this.contractDate)
-			.status(this.status)
-			.user(user)
-			.expectedContractEndDate(this.expectedContractEndDate)
-			.build();
-	}
 }
--- apiserver/service/src/main/java/com/zipline/service/contract/dto/response/ContractListResponseDTO.java
+++ apiserver/service/src/main/java/com/zipline/service/contract/dto/response/ContractListResponseDTO.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 
+import com.zipline.entity.contract.Contract;
 import com.zipline.entity.contract.CustomerContract;
 import com.zipline.entity.enums.ContractStatus;
 
@@ -31,12 +32,24 @@ public class ContractListResponseDTO {
 	@Getter
 	public static class ContractListDTO {
 		private Long uid;
-		private String customerName;
+		private String lessorOrSellerName;
+		private String lesseeOrBuyerName;
 		private String category;
 		private LocalDate contractDate;
 		private LocalDate contractStartDate;
 		private LocalDate contractEndDate;
 		private ContractStatus status;
+
+		public ContractListDTO(Contract contract, List<CustomerContract> customerContracts) {
+			this.uid = contract.getUid();
+			this.category = contract.getCategory();
+			this.contractDate = contract.getContractDate();
+			this.contractStartDate = contract.getContractStartDate();
+			this.contractEndDate = contract.getContractEndDate();
+			this.status = contract.getStatus();
+			this.lessorOrSellerName = customerContracts.get(0).getCustomer().getName();
+			this.lesseeOrBuyerName = findLesseeOrBuyerName(customerContracts);
+		}
 
 		public ContractListDTO(CustomerContract customerContract) {
 			this.uid = customerContract.getContract().getUid();
@@ -46,6 +59,13 @@ public class ContractListResponseDTO {
 			this.contractStartDate = customerContract.getContract().getContractStartDate();
 			this.contractEndDate = customerContract.getContract().getContractEndDate();
 			this.status = customerContract.getContract().getStatus();
+		}
+
+		private String findLesseeOrBuyerName(List<CustomerContract> customerContracts) {
+			if (customerContracts.size() > 1) {
+				return customerContracts.get(1).getCustomer().getName();
+			}
+			return null;
 		}
 	}
 }
