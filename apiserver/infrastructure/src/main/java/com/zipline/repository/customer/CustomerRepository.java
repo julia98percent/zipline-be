ed413ed8446ed7c86e3359ac64ca5b4bc6e1c260
Dsys1129 <dsys1129@gmail.com>
2025-04-29 15:50:54 +0900

    feat: CustomerRepository - 고객의 정보를 필터링해서 조회하는 쿼리 구현

--- apiserver/infrastructure/src/main/java/com/zipline/repository/customer/CustomerRepository.java
+++ apiserver/infrastructure/src/main/java/com/zipline/repository/customer/CustomerRepository.java
@@ -2,22 +2,14 @@ package com.zipline.repository.customer;
 
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.zipline.entity.customer.Customer;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long>, QCustomerRepository {
 	Optional<Customer> findByUidAndDeletedAtIsNull(Long customerUid);
-
-	@Query(value = "SELECT c FROM Customer c " +
-		"WHERE c.user.uid = :userUid AND c.deletedAt IS NULL",
-		countQuery = "SELECT COUNT(c) FROM Customer c WHERE c.user.uid = :userUid AND c.deletedAt IS NULL")
-	Page<Customer> findByUserUidWithLabels(Long userUid, Pageable pageable);
 
 	boolean existsByUidAndUserUidAndDeletedAtIsNull(Long customerUid, Long userUid);
 
