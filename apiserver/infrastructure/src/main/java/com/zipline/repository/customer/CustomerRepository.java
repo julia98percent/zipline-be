402de6431c50a58f3355b5905ebca8ab69caa85c
채지원 <dev.julia.chae@gmail.com>
2025-05-07 19:17:23 +0900

    feat: 생일인 고객을 찾는 쿼리문 추가

--- apiserver/infrastructure/src/main/java/com/zipline/repository/customer/CustomerRepository.java
+++ apiserver/infrastructure/src/main/java/com/zipline/repository/customer/CustomerRepository.java
@@ -1,11 +1,11 @@
 package com.zipline.repository.customer;
 
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.zipline.entity.customer.Customer;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>, QCustomerRepository {
@@ -14,5 +14,9 @@ public interface CustomerRepository extends JpaRepository<Customer, Long>, QCust
 	boolean existsByUidAndUserUidAndDeletedAtIsNull(Long customerUid, Long userUid);
 
 	Optional<Customer> findByUidAndUserUidAndDeletedAtIsNull(Long customerUid, Long userUid);
-}
 
+	@Query("SELECT c FROM Customer c WHERE c.user.uid = :userUid " +
+			"AND SUBSTRING(c.birthday, 5) = :mmdd " +
+			"AND c.deletedAt IS NULL")
+	List<Customer> findCustomersWithBirthdayToday(Long userUid, String mmdd);
+}
