fcd607647e431285252eb29e844561814eb8b2af
typhoon <jungwooshin.97@gmail.com>
2025-05-07 17:56:53 +0900

    (fix)테이블 업데이트 트리거 수정

--- crawlerserver/common/src/main/resources/db/changelog/changes/v1/region/triggers/02-create-after-region-insert-trigger.sql
+++ crawlerserver/common/src/main/resources/db/changelog/changes/v1/region/triggers/02-create-after-region-insert-trigger.sql
@@ -5,22 +5,11 @@ CREATE TRIGGER after_region_insert
 AFTER INSERT ON regions
 FOR EACH ROW
 BEGIN
-    DECLARE migration_exists INT DEFAULT 0;
     IF NEW.level = 3 THEN
-        SELECT COUNT(*) INTO migration_exists
-        FROM migrations
-        WHERE cortar_no = NEW.cortar_no;
-
-        IF migration_exists = 0 THEN
-            INSERT INTO migrations (
-                cortar_no,
-                naver_status,
-                zigbang_status
-            ) VALUES (
-                NEW.cortar_no,
-                'NEW',
-                'NEW'
-            );
-        END IF;
+        INSERT IGNORE INTO migrations (cortar_no, naver_status, zigbang_status)
+        VALUES (NEW.cortar_no, 'NEW', 'NEW');
     END IF;
+
+    INSERT IGNORE INTO crawls (cortar_no, naver_status, zigbang_status)
+    VALUES (NEW.cortar_no, 'NEW', 'NEW');
 END//
