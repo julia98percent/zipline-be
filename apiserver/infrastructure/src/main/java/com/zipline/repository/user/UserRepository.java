dc3aa26b2405c954eef1bd5ba9d76f128930e0f2
채지원 <dev.julia.chae@gmail.com>
2025-05-08 18:50:46 +0900

    feat: 계약 만료 알림 문자 발송 기능 구현에 필요한 dto 및 쿼리 추가

--- apiserver/infrastructure/src/main/java/com/zipline/repository/user/UserRepository.java
+++ apiserver/infrastructure/src/main/java/com/zipline/repository/user/UserRepository.java
@@ -1,12 +1,12 @@
 package com.zipline.repository.user;
 
+import com.zipline.entity.user.User;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import com.zipline.entity.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	@Query("SELECT user FROM User user WHERE user.id = :id")
@@ -15,4 +15,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByNameAndEmail(String name, String email);
 
 	boolean existsById(String id);
+
+	List<User> findByDeletedAtIsNull();
+
 }
