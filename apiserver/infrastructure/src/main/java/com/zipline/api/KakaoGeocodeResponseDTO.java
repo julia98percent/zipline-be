e32aaed2edd3362596ad347b9a50ff6e49fc484d
Dsys1129 <dsys1129@gmail.com>
2025-05-12 19:37:38 +0900

    feat: Kakao Api 연동 - 엑셀에 입력되는 지역 값을 받아 해당 지역의 법정동 주소와 위도, 경도를 받음

---
+++ apiserver/infrastructure/src/main/java/com/zipline/api/KakaoGeocodeResponseDTO.java
@@ -0,0 +1,41 @@
+package com.zipline.api;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+
+@Getter
+public class KakaoGeocodeResponseDTO {
+
+	private List<Document> documents;
+
+	@Getter
+	public static class Document {
+		@JsonProperty("x")
+		private String longitude;
+		@JsonProperty("y")
+		private String latitude;
+
+		@JsonProperty("address")
+		private Address address;
+
+		@JsonProperty("road_address")
+		private RoadAddress roadAddress;
+
+		@Getter
+		public static class Address {
+			@JsonProperty("address_name")
+			private String jibunAddressName;
+			@JsonProperty("b_code")
+			private String legalDistrictCode;
+		}
+
+		@Getter
+		public static class RoadAddress {
+			@JsonProperty("address_name")
+			private String roadAddressName;
+		}
+	}
+}
