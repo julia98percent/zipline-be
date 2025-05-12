7f442e773f7d71c838b331014911e47c130ca176
Dsys1129 <dsys1129@gmail.com>
2025-05-12 20:21:06 +0900

    fix: CustomerRepository - 잘못 작성한 쿼리 삭제

--- apiserver/infrastructure/src/main/java/com/zipline/repository/customer/CustomerRepository.java
+++ apiserver/infrastructure/src/main/java/com/zipline/repository/customer/CustomerRepository.java
@@ -3,8 +3,6 @@ package com.zipline.repository.customer;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -23,11 +21,4 @@ public interface CustomerRepository extends JpaRepository<Customer, Long>, QCust
 		"AND SUBSTRING(c.birthday, 5) = :mmdd " +
 		"AND c.deletedAt IS NULL")
 	List<Customer> findCustomersWithBirthdayToday(Long userUid, String mmdd);
-
-	@Query(value = "SELECT DISTINCT c FROM Customer c " +
-		"LEFT JOIN FETCH c.labelCustomers lc " +
-		"LEFT JOIN FETCH lc.label " +
-		"WHERE c.user.uid = :userUid AND c.deletedAt IS NULL",
-		countQuery = "SELECT COUNT(c) FROM Customer c WHERE c.user.uid = :userUid AND c.deletedAt IS NULL")
-	Page<Customer> findByUserUidWithLabels(Long userUid, Pageable pageable);
 }
