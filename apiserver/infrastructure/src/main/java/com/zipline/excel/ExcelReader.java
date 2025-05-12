a7eb5fc7ed2ec9cf8ba00abc671b17cbd96e8ec0
Dsys1129 <dsys1129@gmail.com>
2025-05-12 19:35:59 +0900

    feat: ExcelReader 생성 - Excel File오픈 -> Row를 순회하는 역할

---
+++ apiserver/infrastructure/src/main/java/com/zipline/excel/ExcelReader.java
@@ -0,0 +1,42 @@
+package com.zipline.excel;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.zipline.global.exception.common.FileUploadException;
+import com.zipline.global.exception.common.errorcode.CommonErrorCode;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class ExcelReader {
+
+	public <T> List<T> readExcel(MultipartFile inputStream, ExcelRowMapper<T> mapper) {
+		try (InputStream is = inputStream.getInputStream()) {
+			XSSFWorkbook workbook = new XSSFWorkbook(is);
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			DataFormatter formatter = new DataFormatter();
+
+			List<T> list = new ArrayList<>();
+			for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
+				Row row = sheet.getRow(i);
+				if (row != null) {
+					list.add(mapper.map(row, formatter));
+				}
+			}
+			return list;
+		} catch (IOException e) {
+			throw new FileUploadException(CommonErrorCode.FILE_UPLOAD_FAILED);
+		}
+	}
+}
