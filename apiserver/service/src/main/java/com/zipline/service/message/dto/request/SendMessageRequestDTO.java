9e425c8f5eab58179d4f441699cba76baadf0e8c
채지원 <dev.julia.chae@gmail.com>
2025-05-15 17:06:27 +0900

    feat: 문자 발송 시 관심 지역 변수 치환을 서버 사이드에서 처리하도록 코드 추가

--- apiserver/service/src/main/java/com/zipline/service/message/dto/request/SendMessageRequestDTO.java
+++ apiserver/service/src/main/java/com/zipline/service/message/dto/request/SendMessageRequestDTO.java
@@ -1,10 +1,18 @@
 package com.zipline.service.message.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SendMessageRequestDTO {
   private String from;
   private String to;
   private String text;
+
+  public SendMessageRequestDTO withText(String text) {
+    return new SendMessageRequestDTO(this.from, this.to, text);
+  }
 }
