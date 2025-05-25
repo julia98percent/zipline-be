7adf875482d9d3846b9362a2c9913c7c0ddba0bb
채지원 <dev.julia.chae@gmail.com>
2025-05-25 16:28:39 +0900

    Remove: crawlerserver 폴더 제거

--- crawlerserver/infrastructure/src/main/java/com/zipline/infrastructure/crawl/fetch/Fetcher.java
+++
@@ -1,8 +0,0 @@
-package com.zipline.infrastructure.crawl.fetch;
-
-import com.zipline.infrastructure.crawl.fetch.dto.FetchConfigDTO;
-
-public interface Fetcher {
-    String fetch(String url, FetchConfigDTO config) throws Exception;
-    String fetchPost(String url, String body, FetchConfigDTO config) throws Exception;
-}
