b946b2df02b731ca6e60d6d87029541e2d01df17
채지원 <dev.julia.chae@gmail.com>
2025-05-28 00:52:57 +0900

    Refactor: 기존의 Notification -> Emitter로 분리

---
+++ apiserver/infrastructure/src/main/java/com/zipline/repository/notification/EmitterRepository.java
@@ -0,0 +1,26 @@
+package com.zipline.repository.notification;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Repository
+public class EmitterRepository {
+
+  private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+  public SseEmitter save(String id, SseEmitter emitter) {
+    emitters.put(id, emitter);
+    return emitter;
+  }
+
+  public void deleteById(String id) {
+    emitters.remove(id);
+  }
+
+  public Map<String, SseEmitter> findAll() {
+    return emitters;
+  }
+
+}
