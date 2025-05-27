f7ed0ea6b1ffb0e9d30d8fde8eb20493f05c288a
채지원 <dev.julia.chae@gmail.com>
2025-05-28 00:54:37 +0900

    Chore: NotificationResponseDTO 추가

---
+++ apiserver/service/src/main/java/com/zipline/service/notification/dto/response/NotificationResponseDTO.java
@@ -0,0 +1,39 @@
+package com.zipline.service.notification.dto.response;
+
+import com.zipline.entity.enums.NotificationCategory;
+import com.zipline.entity.notification.Notification;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@AllArgsConstructor
+@Getter
+@Builder
+public class NotificationResponseDTO {
+
+  private NotificationCategory category;
+  private String content;
+  private boolean read;
+  private String url;
+  private LocalDateTime createdAt;
+
+
+  public static NotificationResponseDTO from(Notification notification) {
+    String urlVariable = String.valueOf(switch (notification.getCategory()) {
+      case BIRTHDAY_MSG, CONTRACT_EXPIRED_MSG -> notification.getMessageHistory().getGroupUid();
+      case NEW_SURVEY -> notification.getSurveyResponse().getUid();
+      case SCHEDULE -> null;
+    });
+
+    return NotificationResponseDTO.builder()
+        .category(notification.getCategory())
+        .read(notification.isRead())
+        .createdAt(notification.getCreatedAt())
+        .content(notification.getContent())
+        .url(urlVariable)
+        .build();
+  }
+}
