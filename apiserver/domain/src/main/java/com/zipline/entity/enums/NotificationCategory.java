560768ae831a640627b95d97ebf31dd2e6f1c818
채지원 <dev.julia.chae@gmail.com>
2025-05-28 00:53:48 +0900

    Feat: Notification 내역 저장을 위해 entity, repository 생성

---
+++ apiserver/domain/src/main/java/com/zipline/entity/enums/NotificationCategory.java
@@ -0,0 +1,8 @@
+package com.zipline.entity.enums;
+
+public enum NotificationCategory {
+  NEW_SURVEY,
+  BIRTHDAY_MSG,
+  CONTRACT_EXPIRED_MSG,
+  SCHEDULE;
+}
--- apiserver/infrastructure/src/main/java/com/zipline/repository/notification/NotificationRepository.java
+++ apiserver/infrastructure/src/main/java/com/zipline/repository/notification/NotificationRepository.java
@@ -1,7 +1,10 @@
 package com.zipline.repository.notification;
 
+import com.zipline.entity.notification.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
 }
