dbae7e73f2e7061644853a07f4cbb98e96ccec6b
채지원 <dev.julia.chae@gmail.com>
2025-05-30 18:40:30 +0900

    Feat: 문자 상세 내역 호출 api 추가

--- apiserver/controller/src/main/java/com/zipline/controller/message/MessageController.java
+++ apiserver/controller/src/main/java/com/zipline/controller/message/MessageController.java
@@ -5,6 +5,7 @@ import com.zipline.global.response.ApiResponse;
 import com.zipline.service.message.MessageService;
 import com.zipline.service.message.dto.request.MessageHistoryRequestDTO;
 import com.zipline.service.message.dto.response.MessageHistoryResponseDTO;
+import com.zipline.service.message.dto.response.MessageListResponseDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.security.Principal;
 import java.util.List;
@@ -12,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,6 +44,16 @@ public class MessageController {
 
     ApiResponse<MessageHistoryResponseDTO> result = ApiResponse.ok("문자 발송 내역 조회에 성공했습니다.",
         messageService.getMessageHistory(requestDTO, Long.parseLong(principal.getName())));
+    return ResponseEntity.status(HttpStatus.OK).body(result);
+  }
+
+  @GetMapping("/{messageGroupUid}")
+  public ResponseEntity<ApiResponse<MessageListResponseDTO>> getMessageList(
+      @PathVariable String messageGroupUid,
+      Principal principal) {
+
+    ApiResponse<MessageListResponseDTO> result = ApiResponse.ok("문자 발송 상세 내역 조회에 성공했습니다.",
+        messageService.getMessageList(messageGroupUid, Long.parseLong(principal.getName())));
     return ResponseEntity.status(HttpStatus.OK).body(result);
   }
 
