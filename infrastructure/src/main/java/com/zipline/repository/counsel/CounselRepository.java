6635d3bbc9624c5eda47e2d63495a35acecd1b50
채지원 <dev.julia.chae@gmail.com>
2025-07-04 22:54:17 +0900

    Chore: 사용하지 않는 쿼리 제거

--- infrastructure/src/main/java/com/zipline/repository/counsel/CounselRepository.java
+++ infrastructure/src/main/java/com/zipline/repository/counsel/CounselRepository.java
@@ -1,21 +1,21 @@
 package com.zipline.repository.counsel;
 
 import com.zipline.entity.counsel.Counsel;
-import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CounselRepository extends JpaRepository<Counsel, Long>, QCounselRepository {
-	Optional<Counsel> findByUidAndUserUidAndDeletedAtIsNull(Long uid, Long userUid);
 
-	List<Counsel> findByCustomerUidAndUserUidAndDeletedAtIsNullOrderByCreatedAtDesc(Long customerUid, Long userUid);
+  Optional<Counsel> findByUidAndUserUidAndDeletedAtIsNull(Long uid, Long userUid);
 
-	Page<Counsel> findByCustomerUidAndUserUidAndDeletedAtIsNullOrderByCreatedAtDesc(Long customerUid, Long userUid, Pageable pageable);
 
-	@Query("SELECT c FROM Counsel c JOIN FETCH c.customer WHERE c.user.uid = :userUid AND c.agentProperty.uid = :propertyUid AND c.deletedAt IS NULL ORDER BY c.counselDate desc")
-	Page<Counsel> findByUserUidAndAgentPropertyUidAndDeletedAtIsNull(Long userUid, Long propertyUid, Pageable pageable);
+  Page<Counsel> findByCustomerUidAndUserUidAndDeletedAtIsNullOrderByCreatedAtDesc(Long customerUid,
+      Long userUid, Pageable pageable);
+
+  @Query("SELECT c FROM Counsel c JOIN FETCH c.customer WHERE c.user.uid = :userUid AND c.agentProperty.uid = :propertyUid AND c.deletedAt IS NULL ORDER BY c.counselDate desc")
+  Page<Counsel> findByUserUidAndAgentPropertyUidAndDeletedAtIsNull(Long userUid, Long propertyUid,
+      Pageable pageable);
 }
